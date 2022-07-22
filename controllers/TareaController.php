524595956e96bd51681786cf8a67150542f5b659
Andrés Nehuel <villagraandres00@gmail.com>
2022-07-22 16:24:05 -0500

    lista de tareas

--- controllers/TareaController.php
+++ controllers/TareaController.php
@@ -60,7 +60,8 @@ class TareaController{
             $respuesta=[
                 'tipo'=> 'exito',
                 'id' => $resultado['id'],
-                'mensaje'=>'Tarea creada correctamente'
+                'mensaje'=>'Tarea creada correctamente',
+                'proyectoId'=>$proyecto->id
               ];
               
            echo json_encode($respuesta);
--- views/dashboard/proyecto.php
+++ views/dashboard/proyecto.php
@@ -5,7 +5,12 @@
     <div class="contenedor-nueva-tarea">
         <button type="button" class="agregar-tarea" id="agregar-tarea" > &#43; Nueva Tarea</button>
     </div>
+
+    <ul id="listado-tareas" class="listado-tareas">
+        
+    </ul>
 </div>
+
 
 
 <?php include_once __DIR__. '/footer.php' ?>
